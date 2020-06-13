058e6a7ede59fb9552ba8535578c8dbf581a7618
Arthur Anglade <arthur.anglade@rts.ch>
2020-06-13 15:17:50 +0200

    first stat collection working

--- aws_lambda/stats_handler.py
+++ aws_lambda/stats_handler.py
@@ -1,3 +1,20 @@
+import os
+import requests
+
 def stats_handler(event, context):
+    ## oauth2 ##
+    token = requests.post(url=f"https://id.twitch.tv/oauth2/token?client_id={os.environ['TWITCH_CLIENT_ID']}&client_secret={os.environ['TWITCH_CLIENT_SECRET']}&grant_type=client_credentials").json()
+        #TODO:Check si on en a deja un, sinon le stocker
     
-    return 
+    headers = {
+        'Client-ID':'gddbva13mup10zamsdju7o8qtnxs7t',
+        'Authorization':f'Bearer {token["access_token"]}'
+    }
+
+    followers = requests.get(url="https://api.twitch.tv/helix/users/follows?to_id=456444689",headers=headers).json()
+    stream = requests.get(url="https://api.twitch.tv/helix/streams?user_login=rtsesport_tv",headers=headers).json()
+    print(stream)
+    return
+
+if __name__ == '__main__':
+    stats_handler("dummy","dummy")
