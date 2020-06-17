616f7f39c303cd8e4eb1ee72ec14c03f58ba0811
Arthur Anglade <arthur.anglade@rts.ch>
2020-06-17 17:38:10 +0200

    clean vars for github

--- aws_lambda/stats_handler.py
+++ aws_lambda/stats_handler.py
@@ -4,22 +4,27 @@ import redis
 
 def stats_handler(event, context):
     REDIS_URL = os.environ.get('REDIS_URL')
+    TWITCH_CLIENT_ID = os.environ.get('TWITCH_CLIENT_ID')
+    TWITCH_CLIENT_SECRET = os.environ.get('TWITCH_CLIENT_SECRET')
+    TWITCH_CHANNEL_ID = os.environ.get('TWITCH_CHANNEL_ID')
+    TWITCH_CHANNEL_NAME = os.environ.get('TWITCH_CHANNEL_NAME')
+
     r = redis.StrictRedis(host=REDIS_URL, decode_responses=True)
 
     ## oauth2 ##
     token = r.hgetall("token")
     if token == {} or r.ttl("token") < 60:
-        token = requests.post(url=f"https://id.twitch.tv/oauth2/token?client_id={os.environ['TWITCH_CLIENT_ID']}&client_secret={os.environ['TWITCH_CLIENT_SECRET']}&grant_type=client_credentials").json()
+        token = requests.post(url=f"https://id.twitch.tv/oauth2/token?client_id={TWITCH_CLIENT_ID}&client_secret={TWITCH_CLIENT_SECRET}&grant_type=client_credentials").json()
         r.hmset("token",token)
         r.expire("token",token["expires_in"])
     
     headers = {
-        'Client-ID':os.environ['TWITCH_CLIENT_ID'],
+        'Client-ID':TWITCH_CLIENT_ID,
         'Authorization':f'Bearer {token["access_token"]}'
     }
 
-    followers = requests.get(url="https://api.twitch.tv/helix/users/follows?to_id=456444689",headers=headers).json()
-    stream = requests.get(url="https://api.twitch.tv/helix/streams?user_login=rtsesport_tv",headers=headers).json()
+    followers = requests.get(url=f"https://api.twitch.tv/helix/users/follows?to_id={TWITCH_CHANNEL_ID}",headers=headers).json()
+    stream = requests.get(url=f"https://api.twitch.tv/helix/streams?user_login={TWITCH_CHANNEL_NAME}",headers=headers).json()
     print(stream)
     return
 
